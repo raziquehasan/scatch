f5de11e29dd078f8cc39e83141e4903ba176f4be
raziquehasan <164611217+raziquehasan@users.noreply.github.com>
2025-08-10 16:15:47 +0530

    update

---
+++ models/owner.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose")
+
+
+
+
+
+const ownerSchema =  mongoose.Schema({
+  fullname: {
+    type: String,
+    minLength:3,
+    trim: true,
+
+  },
+  email: String,
+  password: String,
+ 
+  isadmin:Boolean,
+  products : {
+    type: Array,
+    default: []
+  },
+ 
+  picture:String,
+  gstin:String,
+
+});
+
+module.exports  = mongoose.model("Owner", ownerSchema)
+
